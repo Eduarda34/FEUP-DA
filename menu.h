35fe1977d65af10b0da73c69be6bbcf41d2bc27e
Ricardo Silva <ricardo-jorge-silva@outlook.com>
2024-03-06 09:09:20 +0000

    Reader e menu

---
+++ menu.h
@@ -0,0 +1,18 @@
+//
+// Created by ricar on 06/03/2024.
+//
+
+#pragma once
+
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+class Menu {
+public:
+
+    Menu();
+
+    static void start();
+};
