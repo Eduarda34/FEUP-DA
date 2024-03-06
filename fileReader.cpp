1ba1595490782d764616021c5739778b990d7722
Ricardo Silva <ricardo-jorge-silva@outlook.com>
2024-03-06 09:40:22 +0000

    Started file reader

--- fileReader.cpp
+++ fileReader.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "fileReader.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+fileReader::fileReader() = default;
+
+void fileReader::readFile(const std::string &filename, const std::string &delimiter) {
+
+}
--- fileReader.h
+++ fileReader.h
@@ -2,4 +2,27 @@
 // Created by ricar on 06/03/2024.
 //
 
+#pragma once
 
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class fileReader {
+
+private:
+    std::string filename;
+    std::string delimiter = ",";
+public:
+
+    fileReader();
+
+    readFile(const std::string& filename, const std::string& delimiter);
+
+    std::vector<std::vector<std::string>> getData();
+
+};
